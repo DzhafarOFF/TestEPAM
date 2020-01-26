34cfdf1f020bb12c506969e70c27cbcd6e54b67c
Dzhafar <djafarrn@gmail.com>
2020-01-26 17:11:47 +0300

    fix: cors error

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ const capitalizer = (str) => {
 }
 
 const getData = async () => {
-    const url = 'http://api.randomuser.me/1.0/?results=50&nat=gb,us&inc=gender,name,location,email,phone,picture';
+    const url = 'https://api.randomuser.me/1.0/?results=50&nat=gb,us&inc=gender,name,location,email,phone,picture';
     const response = await fetch(url).then(response => response.json());
     return response.results;
 }
